83440688c13ed0d494d95f83885a851c984395eb
FabienBarriau <fabienbarriau@ymail.com>
2020-08-07 18:27:56 +0200

    i

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  apiUrl: 'http://0.0.0.0:5000/api/v1/'
+  production: true,
+  apiUrl: 'http://51.75.142.87:5000/api/v1/'
 };
 
 /*
