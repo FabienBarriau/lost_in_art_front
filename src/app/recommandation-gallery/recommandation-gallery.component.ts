d83b38d95b5cf17a9723dd94a0eb07e44bb8ca4c
FabienBarriau <fabienbarriau@ymail.com>
2021-02-27 01:29:30 +0100

    nogutter -> no more weird blank p-grid

--- src/app/recommandation-gallery/recommandation-gallery.component.ts
+++ src/app/recommandation-gallery/recommandation-gallery.component.ts
@@ -11,6 +11,24 @@ export class RecommandationGalleryComponent implements OnInit {
 
   imgs = [];
   _activeIndex: number = 0;
+  responsiveOptions:any[] = [
+      {
+          breakpoint: '1024px',
+          numVisible: 5
+      },
+      {
+          breakpoint: '960px',
+          numVisible: 4
+      },
+      {
+          breakpoint: '768px',
+          numVisible: 3
+      },
+      {
+          breakpoint: '560px',
+          numVisible: 1
+      }
+  ];
 
   constructor(private recommandationService: RecommandationService) {
     this.recommandationService.imgs.subscribe(valeur => {
